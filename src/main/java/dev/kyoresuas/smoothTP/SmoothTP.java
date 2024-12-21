e951f7c6dcd092c9532c210375ad082dc3b5d380
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2024-12-21 15:35:40 +0900

    Initial Commit

---
+++ src/main/java/dev/kyoresuas/smoothTP/SmoothTP.java
@@ -0,0 +1,17 @@
+package dev.kyoresuas.smoothTP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmoothTP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
